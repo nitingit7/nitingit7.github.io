ed1460dd56b6103d70e1272bd7e4cad5305f0c32
Nitin <119484935+nitingit7@users.noreply.github.com>
2025-12-10 20:11:28 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,25 @@
+// js/config.js
+var BASE_URL = "https://nitingit7.github.io/Mock-Test/";
+
+var GITHUB_FILES = {
+    'quant': [
+        { name: "Testing", file: BASE_URL + "tests/quant/mock1.json" },
+        { name: "RRB Clerk Speed Math 1", file: BASE_URL + "tests/quant/mock2.json" },
+        { name: "RRB Clerk Quant: [Easy]", file: BASE_URL + "tests/quant/mock3.json" },
+        { name: "RRB Clerk Speed Math 2", file: BASE_URL + "tests/quant/SM2.json" },
+        { name: "RRB Clerk Prelims: Quant [Moderate]", file: BASE_URL + "tests/quant/hardqa1.json" },
+        { name: "RRB Clerk Prelims: Quant [Easy-Moderate]", file: BASE_URL + "tests/quant/quanteasymod.json" },
+        { name: "RRB Clerk Prelims: Quant [Easy-Moderate] 2", file: BASE_URL + "tests/quant/easytomod2.json" },
+        { name: "RRB Clerk Prelims: Quant [Moderate]", file: BASE_URL + "tests/quant/quant5.json" }
+    ],
+    'reasoning': [
+        { name: "RRB Clerk Reasoning: Puzzle & Seating", file: BASE_URL + "tests/reasoning/mock1.json" },
+        { name: "RRB Clerk Reasoning: Sectional 1", file: BASE_URL + "tests/reasoning/reas2.json" },
+        { name: "RRB Clerk Reasoning: Marathon", file: BASE_URL + "tests/reasoning/puzzlemarathon.json" }
+    ],
+    'english': [
+        { name: "COMING SOON....", file: BASE_URL + "tests/english/mock1.json" }
+    ]
+};
+
+var GOOGLE_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfcBe23FbEQkCmH2QEWLUvV2Ei0CFezkOv54BXYG40GheCCLw/viewform?usp=pp_url&entry.57428168=TEST_NAME_HERE&entry.1889905698=Q_NUM_HERE";
