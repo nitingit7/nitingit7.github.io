ee91f752a3e1982f35a2f7c5aaa6379a2d172ed4
Nitin <119484935+nitingit7@users.noreply.github.com>
2025-12-13 23:05:37 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,38 +1,26 @@
 // js/config.js
-var BASE_URL = "https://nitingit7.github.io/Mock-Test/";
+
+// Use "./" to force looking in the current folder
+var BASE_URL = "./"; 
 
 var GITHUB_FILES = {
     'quant': [
-        { name: "Testing", file: BASE_URL + "tests/quant/mock1.json" },
-        { name: "RRB Clerk: Speed Math 1", file: BASE_URL + "tests/quant/mock2.json" },
-        { name: "RRB Clerk Quant: [Easy]", file: BASE_URL + "tests/quant/mock3.json" },
-        { name: "RRB Clerk: Speed Math 2", file: BASE_URL + "tests/quant/SM2.json" },
-        { name: "RRB Clerk Prelims: Quant [Moderate]", file: BASE_URL + "tests/quant/hardqa1.json" },
-        { name: "RRB Clerk Prelims: Quant [Easy-Moderate]", file: BASE_URL + "tests/quant/quanteasymod.json" },
-        { name: "RRB Clerk Prelims: Quant [Easy-Moderate] 2", file: BASE_URL + "tests/quant/easytomode2.json" },
-        { name: "RRB Clerk Prelims: Quant [Moderate]", file: BASE_URL + "tests/quant/quant5.json" },
-        { name: "RRB Clerk: Speed Math [Moderate]", file: BASE_URL + "tests/quant/QA008.json" },
-        { name: "RRB Clerk: Speed Math [Moderate]", file: BASE_URL + "tests/quant/QA009.json" },
-        { name: "RRB Clerk: Speed Math [Moderate]", file: BASE_URL + "tests/quant/QA010.json" },
-        { name: "RRB Clerk Prelims: Quant [Moderate]", file: BASE_URL + "tests/quant/QA011.json" },
-        { name: "RRB Clerk Prelims: Quant [Moderate]", file: BASE_URL + "tests/quant/QA012.json" },
-        { name: "RRB Clerk Prelims: Quant [Moderate]", file: BASE_URL + "tests/quant/QA013.json" },
-        { name: "RRB Clerk Prelims Quant (7th Dec Shift 1)", file: BASE_URL + "tests/quant/QA014.json" },
-        { name: "RRB Clerk Prelims: Quant (6th Dec Shift 1)", file: BASE_URL + "tests/quant/QA015.json" },
-        { name: "RRB Clerk Prelims: Quant (6th Dec Shift 2)", file: BASE_URL + "tests/quant/QA016.json" },
-        { name: "RRB Clerk Prelims: Quant (7th Dec Shift 2)", file: BASE_URL + "tests/quant/QA017.json" }
+        { name: "Testing", file: "tests/quant/mock1.json" },
+        { name: "RRB Clerk Speed Math 1", file: "tests/quant/mock2.json" },
+        { name: "RRB Clerk Quant: [Easy]", file: "tests/quant/mock3.json" },
+        { name: "RRB Clerk Speed Math 2", file: "tests/quant/SM2.json" },
+        { name: "RRB Clerk Prelims: Quant [Moderate]", file: "tests/quant/hardqa1.json" },
+        { name: "RRB Clerk Prelims: Quant [Easy-Moderate]", file: "tests/quant/quanteasymod.json" },
+        { name: "RRB Clerk Prelims: Quant [Easy-Moderate] 2", file: "tests/quant/easytomod2.json" },
+        { name: "RRB Clerk Prelims: Quant [Moderate]", file: "tests/quant/quant5.json" }
     ],
     'reasoning': [
-        { name: "RRB Clerk Reasoning: Puzzle & Seating", file: BASE_URL + "tests/reasoning/mock1.json" },
-        { name: "RRB Clerk Reasoning: Sectional 1", file: BASE_URL + "tests/reasoning/reas2.json" },
-        { name: "RRB Clerk Reasoning: Marathon", file: BASE_URL + "tests/reasoning/puzzlemarathon.json" },
-        { name: "RRB Clerk Prelims: Reasoning Ability [Moderate]", file: BASE_URL + "tests/reasoning/RA004.json" },
-        { name: "RRB Clerk Prelims: Reasoning (6th Dec Shift 2)", file: BASE_URL + "tests/reasoning/RA005.json" },
-        { name: "RRB Clerk Prelims: Reasoning (7th Dec Shift 1)", file: BASE_URL + "tests/reasoning/RA006.json" },
-         { name: "RRB Clerk Prelims: Reasoning (xth Dec Shift x)", file: BASE_URL + "tests/reasoning/RA007.json" }
+        { name: "RRB Clerk Reasoning: Puzzle & Seating", file: "tests/reasoning/mock1.json" },
+        { name: "RRB Clerk Reasoning: Sectional 1", file: "tests/reasoning/reas2.json" },
+        { name: "RRB Clerk Reasoning: Marathon", file: "tests/reasoning/puzzlemarathon.json" }
     ],
     'english': [
-        { name: "COMING SOON....", file: BASE_URL + "tests/english/mock1.json" }
+        { name: "COMING SOON....", file: "tests/english/mock1.json" }
     ]
 };
 
