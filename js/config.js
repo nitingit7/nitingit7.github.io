6cddd68a557982ee9a4ad9a2ebce9bc9397b3d8b
Nitin <119484935+nitingit7@users.noreply.github.com>
2025-12-16 13:24:42 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -20,7 +20,8 @@ var GITHUB_FILES = {
         { name: "RRB Clerk Prelims Quant (7th Dec Shift 1)", file: BASE_URL + "tests/quant/QA014.json" },
         { name: "RRB Clerk Prelims: Quant (6th Dec Shift 1)", file: BASE_URL + "tests/quant/QA015.json" },
         { name: "RRB Clerk Prelims: Quant (6th Dec Shift 2)", file: BASE_URL + "tests/quant/QA016.json" },
-        { name: "RRB Clerk Prelims: Quant (7th Dec Shift 2)", file: BASE_URL + "tests/quant/QA017.json" }
+        { name: "RRB Clerk Prelims: Quant (7th Dec Shift 2)", file: BASE_URL + "tests/quant/QA017.json" },
+        { name: "RRB Clerk Full Mock 1", file: BASE_URL + "tests/quant/Full_Mock/RRB/RRBQS001.json" }
     ],
     'reasoning': [
         { name: "RRB Clerk Reasoning: Puzzle & Seating", file: BASE_URL + "tests/reasoning/mock1.json" },
