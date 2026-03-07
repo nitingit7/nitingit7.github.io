2feda07daae71d9d98247796473538186d18a88b
Nitin <119484935+nitingit7@users.noreply.github.com>
2026-03-08 00:16:07 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -91,6 +91,7 @@ var GITHUB_FILES = {
         { name: "Vocab 5", tags: "Vocab", questions: 20, time: 10, marks: 20, file: BASE_URL + "tests/english/EV005.json" },
         { name: "Vocab 6", tags: "Vocab", questions: 20, time: 10, marks: 20, file: BASE_URL + "tests/english/EV006.json" },
         { name: "Vocab 7", tags: "Vocab", questions: 20, time: 10, marks: 20, file: BASE_URL + "tests/english/EV007.json" },
+        { name: "Vocab 8", tags: "Vocab", questions: 20, time: 10, marks: 20, file: BASE_URL + "tests/english/EV008.json" },
     ],
 
     // --- 4. GENERAL AWARENESS ---
